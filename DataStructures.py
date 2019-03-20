53974a8d367f3c66a8ba3655fd337d90ea9cec02
magiczyf <36447303+magiczyf@users.noreply.github.com>
2019-03-20 08:09:00 +0800

    Create DataStructures.py

---
+++ DataStructures.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 07:47:51 2019
+
+@author: zyf
+"""
+
+# 5.1. More on Lists
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('apple')
+fruits.index('banana')
+fruits.index('banana', 4)  # Find next banana starting a position 4
+fruits.reverse()
+fruits.append('grape')
+fruits.sort()
+fruits.pop()
